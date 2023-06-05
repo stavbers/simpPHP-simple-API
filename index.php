f03d891fc789d93604328bb93c953afb16a22875
stavbers <reddevweb@gmail.com>
2023-06-05 23:06:38 +0300

    add city id route

--- index.php
+++ index.php
@@ -26,4 +26,30 @@ $app->get('/city', function () {
   echo json_encode($name, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
+$app->get('/city/{id}', function(\Slim\Http\Request $request, \Slim\Http\Response $response) use($app){
+header('Content-Type: application/json;charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+
+$id = $request->getAttribute('id');
+$city = ORM::for_table('city')->find_one($id);
+
+if(!$city){
+echo json_encode([
+  'data' => [],
+  'code' => 1,
+  'message' => 'no requested information'
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+} else {
+echo json_encode ([
+  'data' => [
+      'id'=> $city->id,
+      'name' => $city->name,
+      'country_code' => $city->country_code,
+      'population'=> $city->population,
+      'img'=> $city->img,
+  ]
+  ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+}
+});
+
 $app->run();
