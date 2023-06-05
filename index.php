b70f56f4c1e84d0eaea64aa3d33dc70d1c22efe6
stavbers <reddevweb@gmail.com>
2023-06-05 23:01:21 +0300

    add homepage route

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php 
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$config = ['settings' => ['displayErrorDetails' => true]]; 
+$app = new \Slim\App($config);
+
+
+$app->get('/', function () {
+  header('Content-Type: application/json;charset=utf-8');
+  header("Access-Control-Allow-Origin: *");
+  echo 'go page /city';
+});
+
+
+$app->run();
