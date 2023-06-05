49c9a327e6b5aa907e5f0a63b5a79692f6434ef1
stavbers <reddevweb@gmail.com>
2023-06-05 23:04:47 +0300

    add data base config

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ require_once __DIR__.'/vendor/autoload.php';
 $config = ['settings' => ['displayErrorDetails' => true]]; 
 $app = new \Slim\App($config);
 
+ORM::configure(array(
+  'connection_string' => 'mysql:host=your-host;dbname=your-dbName',
+  'username'=> 'your-username',
+  'password'=> 'your-pass'
+));
 
 $app->get('/', function () {
   header('Content-Type: application/json;charset=utf-8');
